96128dff99bac22ae5c505a48b4d4af73751a498
unnmd88_h <unnmd88@gmail.com>
2024-11-01 20:20:06 +0300

    create handlers.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import asyncio
+import logging
+
 from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 
