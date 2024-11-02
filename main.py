39bb309fedd755e77d4ca7a116d915060aabc50d
unnmd88_h <unnmd88@gmail.com>
2024-11-02 22:30:24 +0300

    реализован ? для получения данных

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram import F
+from aiogram.utils.formatting import (
+    Bold, as_list, as_marked_section, as_key_value, HashTag
+)
 import logging_cfg
 import services
 
@@ -26,44 +29,28 @@ dp = Dispatcher()
 bot = Bot(token=os.getenv('SDP_BOT'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 url_get_dataAPI = os.getenv("ip_addrAPI")
 
+
 @dp.message(Command("test1"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
 
-async def request_to_api():
-    data = {
-        "data": {"320": {"host_id": "TEst"}, "num_hosts_in_request": 1, "search_in_db": True}
-    }
-
-    data = json.dumps(data)
-
-    headers = {
-        'User-Agent': os.getenv('user_agent'),
-        'Authorization': f'Token {os.getenv("TOKEN")}',
-        "content-type": "application/json"
-    }
-
-    logger.info(data)
-    async with aiohttp.ClientSession() as session:
-        async with session.post("http://127.0.0.1:8000/api/v1/get-data-from-controller-ax/",
-                                headers=headers, data=data) as s:
-            res = await s.text()
-            return res
-
-
-@dp.message(F.text)
+@dp.message(F.text.contains(' ?'))
 async def cmd_test2(message: types.Message):
-
-    msg = message.text
-    if msg.isdigit() or services.check_valid_ipaddr(msg)[0]:
+    msg = message.text.split()
+    if msg[0].isdigit() or services.check_valid_ipaddr(msg[0])[0]:
         req = services.RequestToApi()
-        res = await req.request_to_api(url_get_dataAPI, msg)
+        res = await req.request_to_api(url_get_dataAPI, msg[0])
     else:
         res = 'dsaasdasdsad'
+    res_json = json.loads(res)
+    logger.debug(res_json)
+    logger.debug(type(res_json))
+    logger.debug(type(res))
     logger.debug(res)
+    await message.answer(f'```\n{res}\n```', parse_mode='MarkdownV2')
 
-    await bot.send_message('-1002412371192', f'```\n{res}\n```', parse_mode='MarkdownV2')
+    # await bot.send_message('-1002412371192', f'```\n{res}\n```', parse_mode='MarkdownV2')
 
 
 # dp.message.register(cmd_test2, F.text)
